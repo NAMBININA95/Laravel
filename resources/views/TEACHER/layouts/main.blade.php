d5fb286ae5e657d87c994282d8b1a89041c30a69
Georges Nambinina <g.nambinina@outlook.fr>
2021-04-03 13:40:51 +0300

    Service Injection mandea soa

---
+++ resources/views/TEACHER/layouts/main.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cours Laravel par Georges Nambinina </title>
+{{--    <title>Cours Laravel par @yield('author' ?? 'Georges Nambinina') </title>--}}
+</head>
+<body>
+{{--@yield('sidebar')--}}
+@yield('contenu')
+</body>
+{{--@yield('myscriptJs')--}}
+</html>
